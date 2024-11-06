564a8ef1fd70ea9281713f8fe0726740f36cafa7
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-06 16:42:59 +1100

    Fix delete method on Tree class

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const demoArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let tree1 = new Tree(demoArray);
 
 tree1.insert(4);
+tree1.insert(13);
 
 prettyPrint(tree1.root);
 
 console.log("-------------------");
 
-// tree1.deleteItem(4);
+tree1.deleteItem(tree1.root, 8);
 
-// prettyPrint(tree1.root);
+prettyPrint(tree1.root);
 
 // console.log(tree1.find(18));
--- nodetree.js
+++ nodetree.js
@@ -88,112 +88,140 @@ export class Tree {
     }
   }
 
-  deleteItem(value) {
-    let node = this.root;
-    let prevNode = this.root;
-    while (node) {
-      if (value < node.data) {
-        if (node.left.data === value) {
-          // If node is a leaf node just delete the reference to it
-          if (node.left.left === null && node.left.right === null) {
-            node.left = null;
-            return;
-          }
-          if (node.left.left && !node.left.right) {
-            // If node to the LEFT has a left node and doesn't have a right node
-            node.left = node.left.left;
-            return;
-          } else if (!node.left.left && node.left.right) {
-            // If node to the LEFT has a right node and doesn't have a left node
-            node.left = node.left.right;
-            return;
-          } else if (node.left.left && node.left.right) {
-            let switchNode = node.left;
-            let previous = node.left;
-            while (switchNode.left || switchNode.right) {
-              if (switchNode.left && switchNode.right) {
-                if (switchNode.right.data > switchNode.left.data) {
-                  previous = switchNode;
-                  switchNode = switchNode.left;
-                } else {
-                  previous = switchNode;
-                  switchNode = switchNode.right;
-                }
-              } else if (switchNode.left && !switchNode.right) {
-                previous = switchNode;
-                switchNode = switchNode.left;
-              } else {
-                previous = switchNode;
-                switchNode = switchNode.right;
-              }
-            }
-            node.left.data = switchNode.data;
-            // Find which side leaf node was on and remove it
-            if (previous.left && switchNode.data === previous.left.data) {
-              previous.left = null;
-            } else if (
-              previous.right &&
-              switchNode.data === previous.right.data
-            ) {
-              previous.right = null;
-            }
-            return;
-          }
-        }
-        prevNode = node;
-        node = node.left;
-      } else {
-        if (node.right.data === value) {
-          // If node is a leaf node just delete the reference to it
-          if (node.right.left === null && node.right.right === null) {
-            node.right = null;
-            return;
-          }
-          if (node.right.left && !node.right.right) {
-            // If node to the RIGHT has a left node and doesn't have a right node
-            node.right = node.right.left;
-            return;
-          } else if (!node.right.left && node.right.right) {
-            // If node to the RIGHT has a right node and doesn't have a left node
-            node.right = node.right.right;
-            return;
-          } else if (node.right.left && node.right.right) {
-            let switchNode = node.right;
-            let previous = node.right;
-            while (switchNode.left || switchNode.right) {
-              if (switchNode.left && switchNode.right) {
-                if (switchNode.right.data > switchNode.left.data) {
-                  previous = switchNode;
-                  switchNode = switchNode.left;
-                } else {
-                  previous = switchNode;
-                  switchNode = switchNode.right;
-                }
-              } else if (switchNode.left && !switchNode.right) {
-                previous = switchNode;
-                switchNode = switchNode.left;
-              } else {
-                previous = switchNode;
-                switchNode = switchNode.right;
-              }
-            }
-            node.right.data = switchNode.data;
-            // Find which side leaf node was on and remove it
-            if (previous.left && switchNode.data === previous.left.data) {
-              previous.left = null;
-            } else if (
-              previous.right &&
-              switchNode.data === previous.right.data
-            ) {
-              previous.right = null;
-            }
-            return;
-          }
-        }
-        prevNode = node;
-        node = node.right;
-      }
+  // deleteItem(value) {
+  //   let node = this.root;
+  //   let prevNode = this.root;
+  //   while (node) {
+  //     if (value < node.data) {
+  //       if (node.left.data === value) {
+  //         // If node is a leaf node just delete the reference to it
+  //         if (node.left.left === null && node.left.right === null) {
+  //           node.left = null;
+  //           return;
+  //         }
+  //         if (node.left.left && !node.left.right) {
+  //           // If node to the LEFT has a left node and doesn't have a right node
+  //           node.left = node.left.left;
+  //           return;
+  //         } else if (!node.left.left && node.left.right) {
+  //           // If node to the LEFT has a right node and doesn't have a left node
+  //           node.left = node.left.right;
+  //           return;
+  //         } else if (node.left.left && node.left.right) {
+  //           let switchNode = node.left;
+  //           let previous = node.left;
+  //           while (switchNode.left || switchNode.right) {
+  //             if (switchNode.left && switchNode.right) {
+  //               if (switchNode.right.data > switchNode.left.data) {
+  //                 previous = switchNode;
+  //                 switchNode = switchNode.left;
+  //               } else {
+  //                 previous = switchNode;
+  //                 switchNode = switchNode.right;
+  //               }
+  //             } else if (switchNode.left && !switchNode.right) {
+  //               previous = switchNode;
+  //               switchNode = switchNode.left;
+  //             } else {
+  //               previous = switchNode;
+  //               switchNode = switchNode.right;
+  //             }
+  //           }
+  //           node.left.data = switchNode.data;
+  //           // Find which side leaf node was on and remove it
+  //           if (previous.left && switchNode.data === previous.left.data) {
+  //             previous.left = null;
+  //           } else if (
+  //             previous.right &&
+  //             switchNode.data === previous.right.data
+  //           ) {
+  //             previous.right = null;
+  //           }
+  //           return;
+  //         }
+  //       }
+  //       prevNode = node;
+  //       node = node.left;
+  //     } else {
+  //       if (node.right.data === value) {
+  //         // If node is a leaf node just delete the reference to it
+  //         if (node.right.left === null && node.right.right === null) {
+  //           node.right = null;
+  //           return;
+  //         }
+  //         if (node.right.left && !node.right.right) {
+  //           // If node to the RIGHT has a left node and doesn't have a right node
+  //           node.right = node.right.left;
+  //           return;
+  //         } else if (!node.right.left && node.right.right) {
+  //           // If node to the RIGHT has a right node and doesn't have a left node
+  //           node.right = node.right.right;
+  //           return;
+  //         } else if (node.right.left && node.right.right) {
+  //           let switchNode = node.right;
+  //           let previous = node.right;
+  //           while (switchNode.left || switchNode.right) {
+  //             if (switchNode.left && switchNode.right) {
+  //               if (switchNode.right.data > switchNode.left.data) {
+  //                 previous = switchNode;
+  //                 switchNode = switchNode.left;
+  //               } else {
+  //                 previous = switchNode;
+  //                 switchNode = switchNode.right;
+  //               }
+  //             } else if (switchNode.left && !switchNode.right) {
+  //               previous = switchNode;
+  //               switchNode = switchNode.left;
+  //             } else {
+  //               previous = switchNode;
+  //               switchNode = switchNode.right;
+  //             }
+  //           }
+  //           node.right.data = switchNode.data;
+  //           // Find which side leaf node was on and remove it
+  //           if (previous.left && switchNode.data === previous.left.data) {
+  //             previous.left = null;
+  //           } else if (
+  //             previous.right &&
+  //             switchNode.data === previous.right.data
+  //           ) {
+  //             previous.right = null;
+  //           }
+  //           return;
+  //         }
+  //       }
+  //       prevNode = node;
+  //       node = node.right;
+  //     }
+  //   }
+  // }
+
+  getSuccessor(curr) {
+    curr = curr.right;
+    while (curr !== null && curr.left !== null) {
+      curr = curr.left;
     }
+    return curr;
+  }
+
+  deleteItem(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (node.data > value) {
+      node.left = this.deleteItem(node.left, value);
+    } else if (node.data < value) {
+      node.right = this.deleteItem(node.right, value);
+    } else {
+      if (node.left === null) return node.right;
+      if (node.right === null) return node.left;
+
+      let succ = this.getSuccessor(node);
+      node.data = succ.data;
+      node.right = this.deleteItem(node.right, succ.data);
+    }
+    return node;
   }
 
   find(value) {
